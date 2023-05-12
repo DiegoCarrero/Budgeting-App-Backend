f7053b019b9d91a383e5a69a71a20c32df0da00b
Diegocarrero <diegocar2413@gmail.com>
2023-05-12 10:26:28 -0400

    edit put controller

--- controllers/transactionsControllers.js
+++ controllers/transactionsControllers.js
@@ -35,7 +35,6 @@ transactions.put("/:index", async (req, res) => {
 
   if (transactionsArray[req.params.index]) {
     transactionsArray[req.params.index] = req.body;
-    console.log("PUT route successful", req.body )
     res.status(200).json(transactionsArray[req.params.index]);
   } else {
     res.status(404).json({ error: "Not Found" });
